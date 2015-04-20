7d937fe08d1c06fd40eb7c76fd81063c36f4cdcd
lynnel <lynne.lammers@gmail.com>
2015-04-19 21:21:19 -0500

    Fixing error in yield

--- linux-2.6.32.60/kernel/sched_other_rr.c
+++ linux-2.6.32.60/kernel/sched_other_rr.c
@@ -85,7 +85,7 @@ static void yield_task_other_rr(struct rq *rq)
 		return;
 	}
 	// get current task
-	task_struct* curr = rq->curr;
+	struct task_struct* curr = rq->curr;
 	// reset its time slice to default
 	curr->task_time_slice = other_rr_time_slice;
 	// move to end
